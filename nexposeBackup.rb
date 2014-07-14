78f02df5e9e7b745e1b5dda472e173d69ef36ccb
BrianWGray <brian.w.gray@gmail.com>
2014-07-14 13:32:06 -0400

    Create nexposeBackup.rb
    Requires yaml configuration file.

---
+++ nexposeBackup.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+# Brian W. Gray
+# 07.14.2014
+
+## Script generates a Platform Independant application backup.
+
+require 'yaml'
+require 'nexpose'
+
+include Nexpose
+
+# Default Values
+
+config = YAML.load_file("conf/nexpose.yaml") # From file
+
+@host = config["hostname"]
+@userid = config["username"]
+@password = config["passwordkey"]
+
+
+nsc = Nexpose::Connection.new(@host, @userid, @password)
+puts 'logging into Nexpose'
+
+begin
+    nsc.login
+rescue ::Nexpose::APIError => err
+    $stderr.puts("Connection failed: #{e.reason}")
+    exit(1)
+end
+
+puts 'logged into Nexpose'
+at_exit { nsc.logout }
+
+begin
+    # Check scan activity wait until there are no scans running
+    active_scans = nsc.scan_activity
+	if active_scans.any?
+        puts "Current scan status: #{active_scans.to_s}"
+        sleep(15)
+    end
+end while active_scans.to_s.empty?
+
+time = Time.new
+backupDescription = time.strftime("%Y%m%d")+"_PI_Weekly"
+
+# Base backup code use from https://community.rapid7.com/thread/4687
+# Start the backup
+if active_scans.empty?
+    platform_independent = true
+    puts "Initiating Platform Independent backup to local disk"
+    nsc.backup(platform_independent, backupDescription)
+else
+
+end
+
+
+puts 'Logging out'
+exit
