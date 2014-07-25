8cf8b5616196bbbecdc45ac1ae0b13250f015c7f
BrianWGray <BrianWGray@gmail.com>
2014-07-25 10:06:06 -0400

    much git fail, connection errors corrected

--- dbMaint.rb
+++ dbMaint.rb
@@ -22,7 +22,7 @@ config = YAML.load_file("conf/nexpose.yaml") # From file
 @port = config["port"]
 
 
-nsc = Nexpose::Connection.new(@host, @userid, @password, @port))
+nsc = Nexpose::Connection.new(@host, @userid, @password, @port)
 puts 'logging into Nexpose'
 
 begin
