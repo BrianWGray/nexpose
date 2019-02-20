89f41651ac9336a49b6359218aaaf876c496014f
BrianWGray <BrianWGray@gmail.com>
2019-02-20 17:11:27 -0500

    quick proof of concept using API documentation examples to generate a static asset group.

--- powershell/create_asset_group.ps1
+++ powershell/create_asset_group.ps1
@@ -2,6 +2,7 @@
 
 # Makes PS ignore self signed certs used by internal servers
 # Have someone actually sign this certificate... - BrianWGray
+
 if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type)
 {
 $certCallback = @"
@@ -46,42 +47,45 @@ Remove-Variable unsecureCreds
 $data_type = "application/json"
 
 # URL Definintions
-$hostName = "nexpose-test"
+$hostName = "nexpose-test.example.com"
 $port = "3780"
-$credentialId = 2 
 $url = "https://${hostName}:${port}/api/3/asset_groups/"
 
 # Build headers to send to the API. In this case we set the Basic authentication value
 $table_headers = @{
-    Authorization=("Basic {0}" -f $base64AuthInfo)
+"Content-Type" = $data_type
+Authorization=("Basic {0}" -f $base64AuthInfo)
 }
 
-# If you wanted to build the content for a credential object you could build it like:
-$body = @{
-    description = "A Static Asset Group with Assets that are Linux Assets running Containers (With Low Access Complexity Vulnerabilities) for remediation purposes.",
-    name = "Container Hosts - Linux"
-    searchCriteria = @{
-                    filters = [
-                            @{  field = "operating-system",
-                                operator = "contains",
-                                value= "linux" },
-                            @{  field = "containers",
-                                operator = "are",
-                                value = 0 },
-                            @{  field = "cvss-access-complexity",
-                                operator = "is",
-                                value = "L" }
-                    ];
-                    match = "all"
-    };
-    type = "static"
+# Search filter criteria
+
+$filter = @{ field = "operating-system"; operator = "contains"; value = "linux"}
+
+$filters = @($filter)
+
+# Here we're using straight JSON to prove API / documentation issues 
+$body = @"
+{
+    "description": "A Static Asset Group with Assets that are Linux Assets running Containers (With Low Access Complexity Vulnerabilities) for remediation purposes.",
+    "name": "Container Hosts - Linux",
+    "searchCriteria": {
+        "filters": [
+            { "field": "operating-system", "operator": "contains", "value": "linux" },
+            { "field": "containers", "operator": "are", "value": 0 },
+            { "field": "cvss-access-complexity", "operator": "is", "value": "L" }
+        ],
+        "match": "all"
+    },
+    "type": "static"
 }
 
+"@
 
-# Build PUT Content using the existing credential object 
-$json =  $body | Convertto-JSON
+# Build Post Content using the existing credential object 
+# We don't need to use the convert to JSON in this example but left it for additional testing.
+$json = $body # | Convertto-JSON
 
 # Visual of the json being sent - just for show
 $json
 
-$data = Invoke-RestMethod -Method 'PUT' -Uri $url -ContentType $data_type -Headers $table_headers -Body $json;
+$data = Invoke-RestMethod -Method 'Post' -Uri $url -Headers $table_headers -Body $json
